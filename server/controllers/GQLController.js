0898150c340343c79397a873131fe648fc220e2b
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-03-07 19:57:32 -0500

    changes before the evening

--- server/controllers/GQLController.js
+++ server/controllers/GQLController.js
@@ -1,8 +1,25 @@
+const express = require('express');
+const curl = require('curl');
+const { graphql, buildSchema } = require('graphql');
 
 const GQLController = {};
 
+//j9dhZHx0KsB5BAM1awwi_Hb7MXmkLegg2RdHuw-W
 
-GQLController.getGQLData()
+
+GQLController.getGQLData = (req, res, next) => {
+    const cloudflare_email = 'airbenderosp@gmail.com';
+    const cloudflare_api_token = 'j9dhZHx0KsB5BAM1awwi_Hb7MXmkLegg2RdHuw-W';
+    const payload = {
+        query: {
+            query{
+                viewer {
+                    zones ()
+                }
+            }
+        }
+    }
+}
 
 
 module.exports = GQLController;
