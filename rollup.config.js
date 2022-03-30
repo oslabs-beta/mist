29420efbd9497e11bbfa5091d91f5e966b1c792a
Eric Lemay <ericlemay@Erics-Air.lan>
2022-03-30 19:35:35 -0400

    npm module update

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,9 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess';
+import { config as configDotenv } from 'dotenv';
+
+configDotenv();
 // import dotenv from 'dotenv';
 // import replace from '@rollup/plugin-replace';
 
@@ -46,14 +49,14 @@ export default {
     file: 'public/build/bundle.js',
   },
   plugins: [
-    // replace({
-    //   // 2 level deep object should be stringify
-    //   process: JSON.stringify({
-    //     env: {
-    //       MY_WORKER: 'sample-worker-2',
-    //     },
-    //   }),
-    // }),
+    replace({
+      // 2 level deep object should be stringify
+      process: JSON.stringify({
+        env: {
+          MY_WORKER: 'sample-worker-2',
+        },
+      }),
+    }),
     svelte({
       preprocess: sveltePreprocess(),
       compilerOptions: {
