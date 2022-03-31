3c82cc2bb397e6bbba3a747cfe3de17d7078bccd
KristenWAlthoff <93629704+KristenWAlthoff@users.noreply.github.com>
2022-03-31 09:13:09 -0400

    Update metrics_model.js

--- server/models/metrics_model.js
+++ server/models/metrics_model.js
@@ -2,8 +2,7 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const myURI = process.env.MY_URI;
-const myURI = 'postgres://mowjiius:ZP2R2gX5BXNqh7CWJJQRWs_Gs4s_Vr2M@salt.db.elephantsql.com/mowjiius';
+const myURI = process.env.MY_URI;
 
 const pool = new Pool({
   connectionString: myURI,
