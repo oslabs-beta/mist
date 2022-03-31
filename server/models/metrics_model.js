fc171a7a09d192646967801c3c2289d3a48d8330
anthonymarinated <anthonymarin718@gmail.com>
2022-03-31 17:13:47 -0400

    removed some remaining console logs

--- server/models/metrics_model.js
+++ server/models/metrics_model.js
@@ -10,7 +10,6 @@ const pool = new Pool({
 
 module.exports = {
   query: (text, params, callback) => {
-    console.log('executed query', text);
     return pool.query(text, params, callback);
   },
 };
